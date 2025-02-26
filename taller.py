e4babf3abc95729c1baf727a552bf8f9a281fd22
Julian Beltran <julianbeltran081@gmail.com>
2025-02-26 10:01:20 -0500

    informacion

---
+++ taller.py
@@ -0,0 +1,7 @@
+Nombre = input("¿Cual es su nombre?")
+Apellido = input("¿Cual es tu apellido?")
+Edad = input("¿Cuale es su edad?")
+Ficha = input("¿Numero de ficha?")
+nombre_tecnologo = input("¿Cual es el nombre de tu carrera tecnologica")
+
+print("su nombre es ",Nombre," ,su apellido es ",Apellido," ,su edad es ",Edad," ,la ficha en la cual esta registrada es ",Ficha," y el nombre de su carrera tecnologica es ",nombre_tecnologo)
